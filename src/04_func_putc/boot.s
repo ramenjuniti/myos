ff08bf4edb6b5cd2d7b091a5ac26a2d7657b64b3
ramenjuniti <ramenjuniti@gmail.com>
2020-03-15 19:24:51 +0900

    XYZを出力できるようになった

---
+++ src/04_func_putc/boot.s
@@ -0,0 +1,45 @@
+    BOOT_LOAD equ 0x7C00 ; ブートプログラムのロード位置
+
+    ORG BOOT_LOAD ; ロードアドレスをアセンブラに指示
+
+; マクロ
+%include "../include/macro.s"
+
+; エントリポイント
+entry:
+    ; BPB(BIOS Parameter Block)
+    jmp ipl ; IPLへジャンプ
+    times 90 - ($ - $$) db 0x90
+
+    ; IPL(Initial Program Loader)
+ipl:
+    cli ; 割り込み禁止
+
+    mov ax, 0x0000
+    mov ds, ax
+    mov es, ax
+    mov ss, ax
+    mov sp, BOOT_LOAD
+
+    sti ; 割り込み許可
+
+    mov [BOOT.DRIVE], dl ; ブートドライブを保存
+
+    ; 文字を表示
+    cdecl putc, word 'X'
+    cdecl putc, word 'Y'
+    cdecl putc, word 'Z'
+
+    ; 処理の終了
+    jmp $
+
+ALIGN 2, db 0
+BOOT:        ; ブートドライブに関する情報
+.DRIVE: dw 0 ; ドライブ番号
+
+; モジュール
+%include "../modules/real/putc.s"
+
+; ブートフラグ（先頭512バイトの終了）
+    times 510 - ($ - $$) db 0x00
+    db 0x55, 0xAA

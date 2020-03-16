91d2e70f89253c6272ceff5ceda1cc16f5f5e922
ramenjuniti <ramenjuniti@gmail.com>
2020-03-17 04:06:49 +0900

    reboot実装

--- src/07_reboot/boot.s
+++ src/07_reboot/boot.s
@@ -1,4 +1,5 @@
         BOOT_LOAD equ 0x7C00
+
         ORG BOOT_LOAD
 
 ;  マクロ
@@ -26,35 +27,13 @@ ipl:
 
         ; 文字列を表示
         cdecl puts, .s0
-
-        ; 数値を表示
-        cdecl itoa, 8086, .s1, 8, 10, 0b0001
-        cdecl puts, .s1
-
-        cdecl itoa, 8086, .s1, 8, 10, 0b0011
-        cdecl puts, .s1
-
-        cdecl itoa, -8086, .s1, 8, 10, 0b0001
-        cdecl puts, .s1
-
-        cdecl itoa, -1, .s1, 8, 10, 0b0001
-        cdecl puts, .s1
-
-        cdecl itoa, -1, .s1, 8, 10, 0b0000
-        cdecl puts, .s1
-
-        cdecl itoa, -1, .s1, 8, 16, 0b0000
-        cdecl puts, .s1
-
-        cdecl itoa, 12, .s1, 8, 2, 0b0100
-        cdecl puts, .s1
+        cdecl reboot ; //戻ってこない
 
         ; 処理の終了
         jmp $
 
         ; データ
 .s0     db "Booting...", 0x0A, 0x0D, 0
-.s1     db "--------", 0x0A, 0x0D, 0
 
 ALIGN 2, db 0
 BOOT:
@@ -62,7 +41,7 @@ BOOT:
 
 ;  モジュール
 %include "../modules/real/puts.s"
-%include "../modules/real/itoa.s"
+%include "../modules/real/reboot.s"
 
 ;  ブートフラグ(先頭512バイトの終了)
         times 510 - ($ - $$) db 0x00

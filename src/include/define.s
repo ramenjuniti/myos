a728d1f45fe43363e7be3eb0820f2873da0d39b1
ramenjuniti <ramenjuniti@gmail.com>
2020-03-29 20:34:07 +0900

    kernel読み込めた

--- src/include/define.s
+++ src/include/define.s
@@ -1,7 +1,15 @@
-BOOT_LOAD equ 0x7C00                    ; ブートプログラムのロード位置
+BOOT_SIZE equ (1024 * 8)                    ; ブートコードサイズ
+KERNEL_SIZE equ (1024 * 8)                  ; カーネルサイズ
 
-BOOT_SIZE equ (1024 * 8)                ; ブートコードサイズ
-SECT_SIZE equ (512)                     ; セクタサイズ
-BOOT_SECT equ (BOOT_SIZE / SECT_SIZE)   ; ブートプログラムのセクタ数
+BOOT_LOAD equ 0x7C00                        ; ブートプログラムのロード位置
+BOOT_END equ (BOOT_LOAD + BOOT_SIZE)
+
+SECT_SIZE equ (512)                         ; セクタサイズ
+
+BOOT_SECT equ (BOOT_SIZE / SECT_SIZE)       ; ブートプログラムのセクタ数
+KERNEL_SECT equ (KERNEL_SIZE / SECT_SIZE)
 
 E820_RECORD_SIZE equ 20
+
+KERNEL_LOAD equ 0x0010_1000
+
